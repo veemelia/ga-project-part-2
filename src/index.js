e85568775b909913eab94a9c36acf0f6a89de09b
veemelia <v.mveenaikar@gmail.com>
2021-09-06 09:13:55 +0800

    Overhaul of repo

---
+++ src/index.js
@@ -0,0 +1,46 @@
+const slider = document.querySelector(".slider");
+const previous = document.querySelector(".previous");
+const mainBtn = document.querySelector(".play");
+const next = document.querySelector(".next");
+const mainImg = document.querySelector("#mainImg");
+const audio = document.querySelector("#audio");
+
+const playImg = "../public/play.png";
+const pauseImg = "../public/pause.png";
+
+let isPlaying = false;
+
+const onLoad = () => {
+  const songList = ["adaytoremember", "romantic", "thejazzpiano"];
+  audio.src = `../public/${
+    songList[Math.floor(Math.random() * songList.length)]
+  }.mp3`;
+};
+
+const togglePlay = () => {
+  if (!isPlaying) {
+    audio.play();
+    isPlaying = true;
+    slider.max = audio.duration;
+    mainImg.src = pauseImg;
+  } else {
+    audio.pause();
+    isPlaying = false;
+    mainImg.src = playImg;
+  }
+
+  audio.addEventListener("ended", () => {
+    audio.currentTime = 0;
+    togglePlay();
+  });
+
+  audio.addEventListener("timeupdate", () => {
+    slider.value = audio.currentTime;
+  });
+
+  slider.addEventListener("change", () => {
+    audio.currentTime = slider.value;
+  });
+};
+
+window.onload = onLoad();
